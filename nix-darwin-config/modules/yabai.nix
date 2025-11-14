2b5afd868cc98a8d19969ffa70571056dbe67c98
DERK0CHER <andenmaus@outlook.com>
2025-11-14 12:26:09 +0100

    all

--- nix-darwin-config/modules/yabai.nix
+++ nix-darwin-config/modules/yabai.nix
@@ -1,16 +1,19 @@
+{ config, pkgs, ... }:
 
-services.yabai = {
-  enable = true;
-  package = pkgs.yabai;
-  enableScriptingAddition = true;
+{
+  services.yabai = {
+    enable = true;
+    package = pkgs.yabai;
+    enableScriptingAddition = false;
 
-  extraConfig = ''
-    # --- Focus follows mouse ---
-    yabai -m config focus_follows_mouse autofocus
-    yabai -m config mouse_follows_focus on
-    yabai -m config mouse_modifier fn
+    extraConfig = ''
 
-    yabai -m config mouse_action1 move
-    yabai -m config mouse_action2 resize
-  '';
-};
+      yabai -m config focus_follows_mouse autofocus
+      yabai -m config mouse_follows_focus on 
+
+      yabai -m config mouse_modifier fn
+      yabai -m config mouse_action1 move
+      yabai -m config mouse_action2 resize
+    '';
+  };
+}
